30ad38cf83bd8d70143edd255b541fed7379bdfc
umbertozapay <oneberto@outlook.com>
2025-09-29 08:59:59 -0300

    add coupon logic

--- src/routes/price.ts
+++ src/routes/price.ts
@@ -4,6 +4,7 @@ import { getDataBase } from "../services/db";
 
 type Payload = {
   productId: string;
+  couponCode?: string;
 };
 
 export const getPricesByProduct = async (
@@ -19,11 +20,48 @@ export const getPricesByProduct = async (
       ({ productId }) => productId === body.productId
     );
 
-    if (!prices) {
-      throw new Error();
+    if (!prices?.length) {
+      return res.status(404).json({
+        error: "Prices not found for this product",
+      });
     }
 
-    return res.status(200).json({ prices });
+    const { couponCode } = body;
+
+    if (!couponCode) {
+      return res.status(200).json({ prices });
+    }
+
+    const coupon = dataBase.coupons.find(
+      ({ code }) => code === couponCode.toUpperCase()
+    );
+
+    if (!coupon) {
+      return res.status(404).json({
+        error: "Coupon not found",
+      });
+    }
+
+    const updatePricesByCoupon = prices.map((item) => {
+      const oldValue = item.amount;
+      const amount =
+        coupon.type === "percentage"
+          ? item.amount * ((100 - coupon.value / 100) / 100)
+          : item.amount - coupon.value;
+
+      const installmentAmount = amount / item.installments;
+
+      return {
+        ...item,
+        oldValue,
+        amount,
+        ...(item.installments > 1 && {
+          installmentAmount,
+        }),
+      };
+    });
+
+    return res.status(200).json({ prices: updatePricesByCoupon, coupon });
   } catch (error) {
     return res.status(500).json({
       error: "Something went wrong",
